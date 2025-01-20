7c539d4b7e0aafcde670190b6f97679642cf3355
Nea Kovalainen <neakovalaine@gmail.com>
2025-01-20 23:37:51 +0200

    Exercise 1.5

--- osa1/exrecise1/ex1/src/App.jsx
+++ osa1/exrecise1/ex1/src/App.jsx
@@ -1,25 +1,27 @@
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
       <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
@@ -28,7 +30,7 @@ const Header = (props) => {
   return (
     <div>
       <h1>
-        {props.course}
+        {props.course.name}
       </h1>
     </div>
   )
@@ -36,7 +38,6 @@ const Header = (props) => {
 }
 
 const Content = (props) => {
-  console.log(props.parts[0].name)
   return (
   <div>
     <Part part={props.parts[0].name} exercises={props.parts[0].exercises} />
