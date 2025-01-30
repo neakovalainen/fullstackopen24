e134dcfbf74fe837c76ebb87b9478298d72f905f
Nea Kovalainen <neakovalaine@gmail.com>
2025-01-30 15:43:26 +0200

    Exercise 1.7

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -8,11 +8,28 @@ const Button = (props) => {
   )
 }
 
+const Average = (props) => {
+  const { good, neutral, bad, text } = props
+  const countedAverage = (good - bad) / (good + neutral + bad)
+  return (
+    <p>{text} {countedAverage || 0}</p> // if NaN, returns 0 (|| = or):)
+  )
+}
+
+const Positive = (props) => {
+  const { good, neutral, bad, text } = props
+  const positiveAmount = (good / (good + neutral + bad)) * 100
+  return (
+    <p>{text} {positiveAmount || 0}%</p>
+  )
+}
+
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
   console.log(good, neutral, bad)
+
   return (
     <div>
       <div>
@@ -26,6 +43,9 @@ const App = () => {
         <p>good {good}</p>
         <p>neutral {neutral}</p>
         <p>bad {bad}</p>
+        <p>all {good + neutral + bad}</p>
+        <Average good={good} neutral={neutral} bad={bad} text="average"/>
+        <Positive good={good} neutral={neutral} bad={bad} text="amount of positive feedback"/>
       </div>
     </div>
   )
