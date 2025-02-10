0fba6d15860e83d247d9b783daad30ca11d5c957
Nea Kovalainen <neakovalaine@gmail.com>
2025-02-10 14:04:33 +0200

    Exercise 2.9

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -2,10 +2,15 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: "040-1231244" }
-  ]) 
+    { name: 'Arto Hellas', number: "040-1231244" },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
+
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [currentFilter, setCurrentFilter] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -23,7 +28,6 @@ const App = () => {
     } 
   }
 
-
   const handleNameAddition = (event) => {
     const exists = persons.find(entry => entry.name === event.target.value)
     console.log(persons)
@@ -36,14 +40,32 @@ const App = () => {
     console.log(event.target.value)
     setNewName(event.target.value)
   }
+
   const handleNumberAddition = (event) => {
     console.log(event.target.value)
     setNewNumber(event.target.value)
   }
 
+  const addFiltering = (event) => {
+    setCurrentFilter(event.target.value)
+  }
+
+  const handleFiltering = (value) => {
+    if (value.name.toUpperCase().includes(currentFilter.toUpperCase())) {
+      return value
+    }
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with
+        <input
+        value={currentFilter}
+        onChange={addFiltering}
+        />
+      </div>
       <form onSubmit={addPerson}>
         <div>
           name: 
@@ -64,7 +86,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map(person =>
+        {persons.filter(handleFiltering).map(person =>
           <li key={person.name}>{person.name} {person.number}</li>
         )}
       </ul>
