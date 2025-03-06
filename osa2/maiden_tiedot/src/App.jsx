cf7b2eef49db7a53af806566ef1e72c402fbe44d
Nea Kovalainen <neakovalaine@gmail.com>
2025-03-06 12:51:44 +0200

    Exercise 2.19

--- osa2/maiden_tiedot/src/App.jsx
+++ osa2/maiden_tiedot/src/App.jsx
@@ -15,36 +15,60 @@ const Countries = (props) => {
       </div>
     )
   }
+
   if (filtered.length > 1) {
+
+    if (props.showMore) {
+      console.log(props.showMore)
+      return (
+        <AdditionalInfo filtered={props.showMore} />)
+    }
+
     return (
       <div>
       {props.countries.filter(props.handleFiltering).map(country =>
-        <p key={country.cca2}>
-        {country.name.common}
-        </p>
+        <div key={country.cca2}>
+          <p key={country.cca2}>
+          {country.name.common}
+          </p>
+          <form onSubmit={() => {props.setShowMore(country)}}>
+            <button type="submit">show more</button>
+          </form>
+        </div>
       )}
+
       </div>
     )}
+
+
+
   if (filtered.length == 1) {
     console.log('languages', filtered[0].languages)
     console.log(filtered)
     return (
-      <div>
-        <h2> {filtered[0].name.common} </h2>
-        <p>{filtered[0].capital[0]}</p>
-        <p>area {filtered[0].area}</p>
-        <h3>Languages</h3>
-        <ul>
-          {Object.entries(filtered[0].languages).map(([key, value]) =>
-            <li key={value}>
-              {value}
-            </li>
-          )}
-        </ul>
-        <p id="biggerflag">{filtered[0].flag}</p>
-      </div>
+      <AdditionalInfo filtered={filtered[0]} />
     )
   }
+}
+
+const AdditionalInfo = (props) => {
+  console.log('was called')
+  return (
+    <div>
+    <h2> {props.filtered.name.common} </h2>
+    <p>{props.filtered.capital[0]}</p>
+    <p>area {props.filtered.area}</p>
+    <h3>Languages</h3>
+    <ul>
+      {Object.entries(props.filtered.languages).map(([key, value]) =>
+        <li key={key}>
+          {value}
+        </li>
+      )}
+    </ul>
+    <p id="biggerflag">{props.filtered.flag}</p>
+  </div>
+  )
 }
 
 const CountryFiltering = (props) => {
@@ -62,6 +86,7 @@ const CountryFiltering = (props) => {
 const App = () => {
   const [countries, setCountries] = useState([])
   const [filtering, setFiltering] = useState('')
+  const [showMore, setShowMore] = useState(false)
 
   useEffect(() => {
     console.log('is smth happening?')
@@ -73,12 +98,11 @@ const App = () => {
   }, [])
 
   const addFilter = (event) => {
-    console.log(event.target.value)
     setFiltering(event.target.value)
+    setShowMore(null)
   }
 
   const handleFiltering = (value) => {
-    console.log('filter', value.name.common)
     if (value.name.common?.toUpperCase().includes(filtering.toUpperCase())) {
       return value
     }
@@ -88,7 +112,7 @@ const App = () => {
     <div>
       <h1> welcome to a country archive~</h1>
       <CountryFiltering filtering={filtering} addFilter={addFilter} />
-      <Countries countries={countries} handleFiltering={handleFiltering} />
+      <Countries countries={countries} handleFiltering={handleFiltering} showMore={showMore} setShowMore={setShowMore}/>
     </div>
   )
 
