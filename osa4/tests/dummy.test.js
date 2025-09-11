ac3bb126d7c673304fa19f110fec661af5a93502
neakovalainen <neakovalaine@gmail.com>
2025-09-11 12:48:04 +0300

    exercise 4.5

--- osa4/tests/dummy.test.js
+++ osa4/tests/dummy.test.js
@@ -34,3 +34,37 @@ describe('total likes', () => {
     assert.strictEqual(result, 15)
   })
 })
+
+describe('favourite blog', () => {
+  const bloglist = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 10,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 8,
+      __v: 0
+    }
+  ]
+
+  test('the most liked blog isssss', () => {
+    const result = listHelper.favouriteBlog(bloglist)
+    assert.deepStrictEqual(result, bloglist[1])
+  })
+})
--- osa4/utils/list_helper.js
+++ osa4/utils/list_helper.js
@@ -6,7 +6,22 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes,0)
 }
 
+const favouriteBlog = (blogs) => {
+  const returned =  blogs.reduce((mostliked, blog) => {
+    if (mostliked.likes < blog.likes) {
+      newliked = {
+        ...blog
+      }
+      return newliked
+    } else {
+      return mostliked
+    }
+  }, blogs[0])
+  return returned
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favouriteBlog
 }
