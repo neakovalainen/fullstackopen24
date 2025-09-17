a9a43d4c0f6d9901e9505cd54a444ff150c6ad9a
neakovalainen <neakovalaine@gmail.com>
2025-09-17 13:40:49 +0300

    exercise 4.15

--- osa4/app.js
+++ osa4/app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 const blogRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 
 const app = express()
 
@@ -10,5 +11,5 @@ mongoose.connect(config.mongoUrl)
 
 app.use(express.json())
 app.use('/api/blogs', blogRouter)
-
+app.use('/api/users', usersRouter)
 module.exports = app
--- osa4/utils/config.js
+++ osa4/utils/config.js
@@ -3,6 +3,6 @@ require('dotenv').config()
 const PORT = process.env.PORT
 const mongoUrl = process.env.NODE_ENV === 'test'
   ? process.env.TEST_MONGODB_URI
-  : ProcessingInstruction.ENV.MONGODB_URI
+  : process.env.MONGODB_URI
 
 module.exports = { mongoUrl, PORT }
