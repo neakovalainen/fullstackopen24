4ba4096bceff7902b33c0abb4cb0344859bc5531
neakovalainen <neakovalaine@gmail.com>
2025-09-24 12:51:44 +0300

    exercise 5.10

--- osa4/bloglist-frontend/src/App.jsx
+++ osa4/bloglist-frontend/src/App.jsx
@@ -150,7 +150,7 @@ const App = () => {
               changeUrl={({ target }) => setUrl(target.value)}
             />
           </Togglable>
-          {blogs.map(blog =>
+          {blogs.sort((a, b) => b.likes - a.likes).map(blog =>
             <Blog key={blog.id} blog={blog} user={user} blogs={blogs} setBlogs={setBlogs}/>
           )}
         </div> 
