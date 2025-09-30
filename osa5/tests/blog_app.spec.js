b6effcfa7028efc52a54778cf215a701ce3b6460
neakovalainen <neakovalaine@gmail.com>
2025-09-30 10:54:51 +0300

    exercise 5.18

--- osa5/tests/blog_app.spec.js
+++ osa5/tests/blog_app.spec.js
@@ -22,4 +22,27 @@ describe('Blog app', () => {
 
     await expect(page.getByText('log in from here')).toBeVisible()
   })
+  describe('Log in', () => {
+    test('user can log in with mluukkai', async ({ page }) => {
+      await page.getByRole('button', { name: 'login' }).click()
+
+      await page.getByLabel('username').fill('mluukkai')
+      await page.getByLabel('password').fill('salainen')
+
+      await page.getByRole('button', { name: 'login' }).click()
+
+      await expect(page.getByText('Matti Luukkainen has logged in')).toBeVisible()
+    })
+
+    test('user cannot log in with wrong username', async ({ page }) => {
+      await page.getByRole('button', { name: 'login' }).click()
+
+      await page.getByLabel('username').fill('mlukkai')
+      await page.getByLabel('password').fill('salainen')
+
+      await page.getByRole('button', { name: 'login' }).click()
+
+      await expect(page.getByText('wrong credentials oh noh')).toBeVisible()
+    })
+  })
 })
